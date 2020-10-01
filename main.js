e4c2783a14273c603063fff859f0ba9808b29a0a
david espinal <davidespinal@davids-MacBook-Pro.local>
2020-09-30 21:42:55 -0400

    Completed class lab

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 //Exercise #1
 //Using map, write a function that takes an array of numbers
 //and makes them strings
+function numToString(num) {
+  let numString = num.map((element) => element.toString());
+  return numString;
+}
 
 //Exercise #2
 //You must use forEach for this exercise
 //Write a function that accepts an array and returns a new array
 //with all the values in the array passed to the function doubled
+function doubled(arr) {
+  let newArray = [];
+
+  arr.forEach((element) => newArray.push(element * 2));
+
+  return newArray;
+}
 
 //Exercise #3
 //Write a function called firstAndLast which accepts
@@ -14,6 +25,18 @@
 //Example: ['winning', 'tomato'] => ['wg', 'to']
 //hint: forEach show be used here
 
+function firstAndLast(arr) {
+  let newArray = [];
+  arr.forEach((element) => {
+    newArray.push(element.charAt(0) + element.charAt(element.length - 1));
+  });
+  return newArray;
+}
 //Bonus:
 //Write a for loop that outputs this:
 // 1 1 1 2 2 2 3 3 3 4 4 4
+for (let i = 1; i <= 4; i++) {
+  for (let g = 0; g < 3; g++) {
+    console.log(i);
+  }
+}
